861ce129e69cac8ede5a4e56d9b55c426a840a0e
Douglas <133309975+douglas2569@users.noreply.github.com>
2023-07-06 23:09:07 -0300

    Add files via upload

--- src/views/users/things/show-reserved-object/index.js
+++ src/views/users/things/show-reserved-object/index.js
@@ -1,29 +1,30 @@
 import ModelCategories from '../../../../models/categories/index.js';
 import ModelThings from '../../../../models/things/index.js';
+import ModelEmail from '../../../../models/email/index.js';
 import Controller from '../../../../core/controller/index.js';
 import config from '../../../../../config.js';
 
 import LayoutHeaderContent from '../../../components/headercontent/index.js';
 import LayoutBreadcrumbs from '../../../components/breadcrumbs/index.js';
 
-class ShowReservedThing extends Controller{   
+class ShowThing extends Controller{   
 
     constructor(){  
         super()      ;
         this.modelCategories = new  ModelCategories();
         this.modelThings = new  ModelThings();
+        this.modelEmail = new  ModelEmail();
         this.identifier = this.retrieveURLId();                     
 
     }   
 
     async getThing(){            
-        const thing = await this.modelThings.getReservedById(this.identifier);                           
+        const thing = await this.modelThings.get(this.identifier);                           
         const category = await this.modelCategories.get(thing.result[0].category_id);         
         
         if(!thing.erro && !category.erro){            
-            document.querySelector("#data-id").value = this.identifier;
-
-            document.querySelector("#code").textContent = `N°:${this.identifier}`;   
+            document.querySelector("#data-id").value = this.identifier;            
+            document.querySelector("#code").textContent = `N°: ${this.identifier}`;            
             
             document.querySelector("form img").setAttribute('src', `${config.urlBase}/${thing.result[0].image_address}`);            
 
@@ -39,15 +40,127 @@ class ShowReservedThing extends Controller{
             
             document.querySelector("#returned-status").value = thing.result[0].returned_status;
             
-            document.querySelector("#date").value = thing.result[0].date;
-            
+            document.querySelector("#date").value = thing.result[0].date;   
 
         }else{
             alert(thing.erro);
         }        
         
-    }   
+    } 
+
+    itsMy(){   
+        
+        document.querySelector("#its-my-button").addEventListener("click",(e)=>{    
+            e.preventDefault();            
+           
+           document.querySelector('#send-email-modal').style.display = 'block';
+           document.querySelector('#first-form').style.display = 'none';
+
+        });
+    } 
     
+    unDisabled(){
+        const disabledFields =  document.querySelectorAll('[disabled]');
+        disabledFields.forEach((item)=>{
+            item.removeAttribute('disabled');
+        });
+    }
+    sendEmail(){                
+        document.querySelector("#send-email-button").addEventListener("click",async (e)=>{    
+            e.preventDefault();                 
+            this.unDisabled();           
+
+           let formData = new FormData(document.querySelector('#first-form'));
+                     
+           formData.set('reserved_status',1);            
+           
+           let formEmail = document.querySelector("#send-email-modal form");                      
+           if(!formEmail.to.value){
+             alert('Insira o email');            
+             formEmail.to.focus();
+             return; 
+            }           
+            let formDataEmail = new FormData(); 
+
+            formDataEmail.append('local', formData.get('local'));
+            formDataEmail.append('local', formData.get('local'));
+            formDataEmail.append('id', formData.get('id'));
+            formDataEmail.append('description', formData.get('description'));
+
+            document.querySelector('#send-email-modal').style.display = 'none';             
+            
+           
+            
+            try {            
+                const screenshotTarget = document.querySelector('#canvas');     
+                let canvas2 = await html2canvas(screenshotTarget);
+                let base64image = canvas2.toDataURL("image/jpeg", 1.0); 
+
+                const response = await fetch(base64image);                           
+                let blob = await response.blob();                              
+                
+                formDataEmail.append('qrcodeBlobScreeshot',blob); 
+                            
+            } catch(e) {
+                console.log(e);
+            } 
+
+
+            console.log('enviando email...');          
+            let response = await this.modelEmail.sendEmail(formDataEmail);
+            console.log('email enviado com sucesso!');          
+
+           if(response.error === ''){            
+              this.modelThings.reserve('', formData, 'Reservado');             
+             document.querySelector('.background-modal').style.display = 'block';  
+
+              
+           }else{
+            alert("Algo de errado não está certo.\n "+response.error);
+           }          
+                      
+
+        });
+
+    }
+    
+    generateQrCode(){        
+        
+        const qrcode = new QRCode("qrcode");
+        
+        let url = `${config.urlBase}/src/views/admin/things/thingreserved/?id=${this.identifier}`        
+        if (!this.identifier) {
+          alert("Id não enviado");          
+          return;
+        }
+        
+        qrcode.makeCode(url);
+    }
+
+    async canvas(){        
+
+        let canvas = document.querySelector('#canvas');                    
+        let context = canvas.getContext('2d');
+                
+        canvas.height = 320;            
+        canvas.width = 320;
+        
+        let img = document.querySelector("#qrcode img");
+
+        img.addEventListener('load', function(){
+               context.drawImage(this,35,35);
+               img.style.display = 'none';
+        });
+                
+    }
+
+    confirmScreenQrcodeButton(){
+         document.querySelector('#confirm-screen-qrcode-button').addEventListener('click', ()=>{
+            window.location.href = config.urlBase;  
+         });        
+
+    }
+
     createHeaderContent(){
         const contentHeader = new LayoutHeaderContent();
         contentHeader.create(document.querySelector('header .container'), config.urlBase, false, false,true,true, false);
@@ -59,21 +172,27 @@ class ShowReservedThing extends Controller{
         const values = [];
         
         values.push( {name:'Tela inicial', href:config.urlBase}  );              
-        values.push( {name:'Objeto reservado', href:'#'}  );        
+        values.push( {name:'Objeto', href:this.retrieveURLCurrentPage()}  );        
 
         layoutBreadcrumbs.create(ul, values);
-    } 
+    }   
 
     handleButtonInfo(){
         document.querySelector('.info-button').addEventListener('click',()=>{
             window.location.href = `${config.urlBase}/src/views/users/information`;
         });
     }
+    
 
 }
 
-const showReservedThing = new ShowReservedThing();
-await showReservedThing.getThing();
-showReservedThing.createHeaderContent();
-showReservedThing.createBreadcrumbs();
-showReservedThing.handleButtonInfo();
+const showThing = new ShowThing();
+await showThing.getThing();
+showThing.itsMy(); 
+showThing.sendEmail(); 
+showThing.generateQrCode(); 
+showThing.canvas(); 
+showThing.confirmScreenQrcodeButton(); 
+showThing.createHeaderContent();
+showThing.createBreadcrumbs();
+showThing.handleButtonInfo();
